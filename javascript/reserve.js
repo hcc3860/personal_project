c05a58a66161dec892e7bffe6d70abe36f076937
hcc3860 <hcc3860@gmail.com>
2021-08-18 16:31:40 +0800

    update

--- javascript/reserve.js
+++ javascript/reserve.js
@@ -1,6 +1,10 @@
 var checkIn = document.getElementsByClassName("check-in");
 var checkOut = document.getElementsByClassName("check-out");
+var statusSearch = document.getElementsByClassName("status-search");
+var checkInValue = checkIn.value;
+var checkOutValue = checkOut.value;
+var test = document.getElementsByClassName("result-notive");
 
-function checkInDate() {
-  checkIn.value = "getFullYear()-getMonth()-getDate()" ;
-}
+var d = new Date();
+checkInValue = Date();
+// console.log(checkInValue);
