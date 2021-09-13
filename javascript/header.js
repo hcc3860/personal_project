db3049dfcff802f573b2a04b3b3a944d2a2ae6d4
hcc3860 <hcc3860@gmail.com>
2021-09-13 09:17:03 +0800

    update

---
+++ javascript/header.js
@@ -0,0 +1,26 @@
+// $(function(){
+// // 點擊按鈕，選單縮放
+//   $("header.header div.container button.hamburger").on("click", function(){
+//     $("header.header nav").slideToggle(" -on");
+//   });
+// });
+//
+// $(function(){
+// // hamburger icon 的切換
+//   $("button.hamburger").on("click", function(){
+//     $(this).toggleClass("is-active");
+//   });
+// });
+//
+//
+// let nav = document.getElementsByTagName("nav")[0];
+// let hamburger = document.getElementsByClassName("hamburger--collapse")[0];
+// window.addEventListener("resize", function(){
+//
+//   if(window.innerWidth > 1200) {
+//     nav.style.display = "inline-block";
+//     hamburger.classList.remove('is-active');
+//   } else if (window.innerWidth <= 1200) {
+//     nav.style.display = "none";
+//   }
+// });
